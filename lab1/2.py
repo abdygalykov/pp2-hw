ce86f59ca3a285e54ebaf471dc58842b463af809
Dimash <abdygalykd@gmail.com>
2025-01-23 16:48:12 +0500

    python variables

--- lab1/2.py
+++ lab1/2.py
@@ -1,4 +1,23 @@
-x = 5
-y = "John"
-print(x)
-print(y)
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
