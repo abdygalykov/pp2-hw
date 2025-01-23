ea0fe60c1ee7cc2924913a6e668e5ca50fd21284
Dimash <abdygalykd@gmail.com>
2025-01-23 16:54:50 +0500

    python strings

---
+++ lab1/6.py
@@ -0,0 +1,13 @@
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+
+# a = "Hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+
+b = "Hello, World!"
+print(b[2:5])
