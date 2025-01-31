c4ee78a1d18d6b03a4957f6831dca9b5e7c44449
Dimash <abdygalykd@gmail.com>
2025-01-31 18:21:30 +0500

    lists

---
+++ lab2/lists.py
@@ -0,0 +1,19 @@
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
