313847c1881bd5be57896b77105d5d85c2975f68
Dimash <abdygalykd@gmail.com>
2025-02-01 15:37:12 +0500

    can check first prime from list

--- lab3/func1/4.py
+++ lab3/func1/4.py
@@ -1,11 +1,15 @@
 def filter_prime(nums):
     for x in nums:
+        check = True
+        if x <= 1:
+            check = False
         for i in range(2, int(x**0.5) + 1):
-            if n % i != 0:
-                return x
-                break
+            if x % i == 0:
+                check = False
+        if check == True :
+            return x 
 
 
-nums = [1, 4, 7, 9, 3, 11]
+nums = [1,2 , 4, 7, 9, 3, 11]
 
 print(filter_prime(nums))
