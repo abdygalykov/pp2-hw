5269779fb663d8c7f746ae94429cc39549d32871
Dimash <abdygalykd@gmail.com>
2025-02-08 15:00:41 +0500

    avg

--- lab3/func2/4.py
+++ lab3/func2/4.py
@@ -77,3 +77,13 @@ movies = [
 "category": "Romance"
 }
 ]
+
+def func(movies):
+    sum = 0
+    for i in movies:
+        sum+=i['imdb']
+    return round(sum/len(movies),2)
+
+
+print(func(movies))
+
