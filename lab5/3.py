7f95680a5d0baa31ed2ec047df8df12b965c6a41
Dimash <abdygalykd@gmail.com>
2025-02-22 15:55:29 +0500

    upd

--- lab5/3.py
+++ lab5/3.py
@@ -1,12 +1,12 @@
 import re
 
 def bool(text):
-    x = re.findall("[a-z]_", text)
+    x = re.findall("[a-z]_[a-z]", text)
     if x:
         return True
     else:
         return False
     
-a = "asdada_sdasd"
+a = "asdada_Asdasd"
 
 print(bool(a))
